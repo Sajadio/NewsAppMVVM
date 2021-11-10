4ac5648e48389a9c4f0e56fd9e49c6ea3e54e98a
S-K-A <sjad04969@gmail.com>
2021-11-10 17:34:16 +0300

    ""

--- app/src/main/java/com/example/newsappmvvm/utils/Constant.kt
+++ app/src/main/java/com/example/newsappmvvm/utils/Constant.kt
@@ -1,11 +1,9 @@
 package com.example.newsappmvvm.utils
 
-import android.icu.text.SimpleDateFormat
 import com.example.newsappmvvm.R
 import java.util.*
-import org.ocpsoft.prettytime.PrettyTime
 
-const val API_KEY = "712f8b1496134646a52f5282da844fb6"
+const val API_KEY = "6953a7421bb749c589e687746c85eee8"
 const val BASE_URL = "https://newsapi.org/"
 
 val categories = mutableListOf(
